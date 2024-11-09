59bd96f9f34c42a3b201efd51992c703de4a6489
Georgi Nikolov <gnnikoll@gmail.com>
2024-11-09 15:34:27 +0200

    Initial commit. Created architecture

---
+++ src/AlpineHub/AlpineHub.Web.ViewModels/Error/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlpineHub.Web.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
