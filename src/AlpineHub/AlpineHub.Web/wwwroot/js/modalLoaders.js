68183c4094d2663909c7de00310cd15e194c23d9
Georgi Nikolov <gnnikoll@gmail.com>
2024-12-04 10:16:28 +0200

    Update static files

---
+++ src/AlpineHub/AlpineHub.Web/wwwroot/js/modalLoaders.js
@@ -0,0 +1,53 @@
+﻿
+$(function () {
+    const deleteSlopeButton = $('.delete-slope-button');
+    deleteSlopeButton.on("click", (e) => {
+        const trElement = $(e.target).parent().parent();
+        const id = trElement.find('td:first').text();
+        $.ajax({
+            method: 'GET',
+            url: "/Manager/DeleteSlope",
+            data: { id: id },
+        })
+            .done(function success(data) {
+                $('#deleteSlopeModal').html(data);
+                $('#deleteSlopeModal').modal('show');
+            })
+            .fail(function fail(data) {
+                //TODO handle error
+            });
+    });
+
+    $('#slopeTable tbody').on('click', 'tr', (e) => {
+        const id = $(e.currentTarget).find('td:first').text();
+        $.ajax({
+            method: 'GET',
+            url: '/Slope/GetSlopeById',
+            data: { id: id }
+        })
+            .done(function success(data) {
+                $('#slopeModalBody').html(data);
+                $('#slopeModal').modal('show');
+            })
+            .fail(function fail(error) {
+                //TODO handle error
+            });
+    });
+
+    $('#liftTable tbody').on('click', 'tr', (e) => {
+        const id = $(e.currentTarget).find("td:first").text();
+
+        $.ajax({
+            method: 'GET',
+            url: '/Lift/GetLiftById',
+            data: { id: id }
+        })
+            .done(function success(data) {
+                $('#liftModalBody').html(data);
+                $('#liftModal').modal('show');
+            })
+            .fail(function fail(error) {
+                //TODO handle error
+            });
+    });
+});
